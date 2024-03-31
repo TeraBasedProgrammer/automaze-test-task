93054d55de5213ef22f94b8e4878b413a667fa73
Ilya <ilyadronov2332@gmail.com>
2024-03-31 11:18:42 +0300

    feat: added ts type hints; onClick handler for the delete button

--- front/app/ui/todo_item.tsx
+++ front/app/ui/todo_item.tsx
@@ -1,15 +1,27 @@
-export default function TodoItem({title, priority, is_done}: {title: string, priority: number, is_done: boolean}) {
+export default function TodoItem({
+  title,
+  priority,
+  isDone,
+  onDelete,
+}: {
+  title: string;
+  priority: number;
+  isDone: boolean;
+  onDelete: () => void;
+}) {
   return (
     <div className="bg-violet-600 flex justify-between items-center p-2 rounded-lg shadow-md">
       <div className="flex gap-2">
         <input type="checkbox" />
-        <div className="text-lg">{title} | Priority:{priority}</div>
+        <div className="text-lg">
+          {title} | Priority:{priority}
+        </div>
       </div>
       <div className="flex gap-2">
         <button className="text-xl shadow-md bg-green-600 text-white hover:bg-green-500 rounded-md px-2">
           Edit
         </button>
-        <button className="text-xl shadow-md bg-red-600 text-white hover:bg-red-500 rounded-md px-2">
+        <button onClick={onDelete} className="text-xl shadow-md bg-red-600 text-white hover:bg-red-500 rounded-md px-2">
           Delete
         </button>
       </div>
